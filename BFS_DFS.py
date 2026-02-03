4ef35ac97169610cd33d18305736ac97510c9ddc
Naomi Otero Flores <naomi.oterof@alumno.buap.mx>
2026-02-03 09:29:04 -0600

    UCS Laberinto por pesos

--- BFS_DFS.py
+++ BFS_DFS.py
@@ -1,116 +1,122 @@
 import time
+import heapq #Para ucs
 from collections import deque
 
-# =========================
-# COLORES ANSI
-# =========================
+
 RESET = "\033[0m"
 GREEN = "\033[92m"
 RED = "\033[91m"
 YELLOW = "\033[93m"
 GRAY = "\033[90m"
 
-# =========================
-# LABERINTO DEFAULT
-# =========================
+
 LABERINTO_DEFAULT = """
-#####################
-#S....#.........#...#
-#.##.#.#.#####.#.#.##
-#.#..#.#.....#.#.#.##
-#.#.##.###.#.#.#.#.##
-#.#......#.#.#...#.##
-#.#.####.#.#.#####.##
-#.#.#..#.#.#.....#.##
-#...#..#...#.###.#.##
-###.#######.#...#..##
-#...#.....#.#.#.##.##
-#.###.###.#.#.#...###
-#...#...#.#...#.....#
-###.#.#.#.#####.##.##
-#...#.#.#.....#...###
-#.###.#.###.#.###..##
-#.....#.....#....G.##
-#####################
+###################
+#S..,,..........#G#
+#.###.###########.#
+#...#.......,,,,..#
+#.#.#.#########.#.#
+#.#.#.....~~~~~.#.#
+#.#.###########.#.#
+#.#.............#.#
+#.###############.#
+#.................#
+###################
 """.strip("\n")
 
+#struct para definir los pesos de los simbolos en los laberintos practica2
+COSTOS = {
+    '.': 1,
+    ',': 5,
+    '~': 10
+}
 
-# =========================
-# UTILIDADES
-# =========================
+
+
+
 def cargar_laberinto(texto):
-    return [list(fila) for fila in texto.splitlines()] # Convierte el texto del laberinto en una lista de listas
+    return [list(fila) for fila in texto.splitlines()]
 
 
-def encontrar_posicion(laberinto, simbolo): # Encuentra la posición de un símbolo en el laberinto
-    for i, fila in enumerate(laberinto): # Recorre cada fila del laberinto
-        for j, celda in enumerate(fila): # Recorre cada celda de la fila
-            if celda == simbolo: # Si la celda coincide con el símbolo buscado
-                return (i, j) #devuelve la posicion 
+def encontrar_posicion(laberinto, simbolo):
+    for i, fila in enumerate(laberinto): 
+        for j, celda in enumerate(fila):
+            if celda == simbolo:
+                return (i, j)
     return None
 
 
 def vecinos(laberinto, pos):
-    x, y = pos #posicion actual
-    movimientos = [(-1,0), (1,0), (0,-1), (0,1)] #solo podemos mover arriba, abajo, izquierda, derecha
-    for dx, dy in movimientos: #recorre los movimientos posibles
-        nx, ny = x + dx, y + dy #nueva posicion
-        if 0 <= nx < len(laberinto) and 0 <= ny < len(laberinto[0]): #verifica que la nueva posicion este dentro de los limites del laberinto
-            if laberinto[nx][ny] != '#': #verifica que la  posicion no sea una pared #
-                yield (nx, ny) #devuelve la nueva posicion
+    x, y = pos
+    movimientos = [(-1,0), (1,0), (0,-1), (0,1)]
+    for dx, dy in movimientos:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < len(laberinto) and 0 <= ny < len(laberinto[0]):
+            if laberinto[nx][ny] != '#':
+                yield (nx, ny)
 
 
-# =========================
-# BFS
-# =========================
+#nueva funcion para el calculo del costo de la celda practica2
+def costo_celda(laberinto, pos):
+    x, y = pos
+    simbolo = laberinto[x][y]
+
+    if simbolo == 'S' or simbolo == 'G':
+        return 0  # inicio y meta no cuestan
+
+    return COSTOS.get(simbolo, float('inf'))
+
+#nueva funcion para calcular el costo de los caminos con peso practica2
+def costo_camino(laberinto, camino):
+    if not camino:
+        return float('inf')
+    return sum(costo_celda(laberinto, pos) for pos in camino)
+
+
+
+
 def bfs(laberinto):
-    """
-    Breadth-First Search
-    Garantiza la ruta más corta
-    """
-    inicio = encontrar_posicion(laberinto, 'S') #posicion de inicio
-    meta = encontrar_posicion(laberinto, 'G') #posicion de meta
+    
+    inicio = encontrar_posicion(laberinto, 'S')
+    meta = encontrar_posicion(laberinto, 'G')
 
-    cola = deque([inicio]) #cola para BFS
-    visitados = set([inicio]) #conjunto de nodos visitados
-    padre = {} #diccionario para reconstruir el camino
-    nodos_visitados = 0 #contador de nodos visitados
+    cola = deque([inicio])
+    visitados = set([inicio])
+    padre = {}
+    nodos_visitados = 0
 
-    while cola:
-        actual = cola.popleft() #saca el primer elemento de la cola
-        nodos_visitados += 1 #incrementa el contador de nodos visitados
+    while cola:         #la cola continua mientras haya nodos en la cola, si la cola esta vacia, no hay mas caminos posibles
+        actual = cola.popleft()
+        nodos_visitados = nodos_visitados + 1 #afectada
 
-        if actual == meta: #si se llega a la meta
-            break 
+        if actual == meta:
+            break
 
-        for v in vecinos(laberinto, actual): #|recorre los vecinos del nodo actual
-            if v not in visitados:#si el vecino no ha sido visitado
-                visitados.add(v) #lo marca como visitado 
-                padre[v] = actual #registra el padre del nodo vecino
-                cola.append(v) #lo añade a la cola
+        for v in vecinos(laberinto, actual):
+            if v not in visitados:
+                visitados.add(v)
+                padre[v] = actual
+                cola.append(v)
 
-    # Reconstrucción del camino     
+    # Reconstrucción del camino
     camino = []
-    actual = meta #comienza desde la meta
-    while actual != inicio: #mientras no se llegue al inicio
-        camino.append(actual) #añade el nodo actual al camino
-        actual = padre.get(actual) #obtiene el padre del nodo actual
-        if actual is None: #si no hay padre, no se encontró camino
-            return None, nodos_visitados #devuelve None y el numero de nodos visitados
-    camino.append(inicio) #añade el nodo de inicio al camino
-    camino.reverse() #invierte el camino para que vaya del inicio a la meta
+    actual = meta
+    while actual != inicio:
+        camino.append(actual)
+        actual = padre.get(actual)
+        if actual is None:
+            return None, nodos_visitados
+    camino.append(inicio)
+    camino.reverse()
+    
+    
 
     return camino, nodos_visitados
 
 
-# =========================
-# DFS
-# =========================
+
 def dfs(laberinto):
-    """
-    Depth-First Search
-    No garantiza la ruta más corta
-    """
+  
     inicio = encontrar_posicion(laberinto, 'S')
     meta = encontrar_posicion(laberinto, 'G')
 
@@ -119,38 +125,86 @@ def dfs(laberinto):
     padre = {}
     nodos_visitados = 0
 
-    while pila:
-        actual = pila.pop() #saca el ultimo elemento de la pila
-        nodos_visitados += 1 #incrementa el contador de nodos visitados
+    while pila:                                                                 #se ejecuta mientras haya nodos en la pila, si la pila esta vacia no hay mas caminos
+        actual = pila.pop()
+        nodos_visitados = nodos_visitados + 1 #afectada
 
-        if actual == meta: #si se llega a la meta
+        if actual == meta:
             break
 
-        for v in vecinos(laberinto, actual): #||recorre los vecinos del nodo actual
-            if v not in visitados: #si el vecino no ha sido visitado
-                visitados.add(v) #lo marca como visitado
-                padre[v] = actual #registra el padre del nodo vecino
-                pila.append(v) #lo añade a la pila
+        for v in vecinos(laberinto, actual):
+            if v not in visitados:
+                visitados.add(v)
+                padre[v] = actual
+                pila.append(v)
+
     # Reconstrucción del camino
     camino = []
     actual = meta
     while actual != inicio:
-        camino.append(actual) #añade el nodo actual al camino
-        actual = padre.get(actual) #obtiene el padre del nodo actual
+        camino.append(actual)
+        actual = padre.get(actual)
         if actual is None:
-            return None, nodos_visitados #devuelve None y el numero de nodos visitados
+            return None, nodos_visitados
     camino.append(inicio)
     camino.reverse()
 
     return camino, nodos_visitados
 
 
-# =========================
-# MOSTRAR LABERINTO
-# =========================
+#algoritmo de costos uniformes
+def ucs(laberinto):
+    inicio = encontrar_posicion(laberinto, 'S')
+    meta = encontrar_posicion(laberinto, 'G')
+
+    cola = []
+    heapq.heappush(cola, (0, inicio))
+
+    costos = {inicio: 0}
+    padre = {}
+    visitados = set()
+    nodos_visitados = 0
+
+    while len(cola) > 0:
+        costo_actual, actual = heapq.heappop(cola)
+
+        if actual in visitados:
+            continue
+
+        visitados.add(actual)
+        nodos_visitados = nodos_visitados + 1
+
+        if actual == meta:
+            break
+
+        for v in vecinos(laberinto, actual):
+            nuevo_costo = costo_actual + costo_celda(laberinto, v)
+
+            if v not in costos or nuevo_costo < costos[v]:
+                costos[v] = nuevo_costo
+                padre[v] = actual
+                heapq.heappush(cola, (nuevo_costo, v))
+
+
+    #reconstruccion del camino
+    camino = []
+    actual = meta
+    while actual != inicio:
+        camino.append(actual)
+        actual = padre.get(actual)
+        if actual is None:
+            return None, nodos_visitados, float('inf')
+    camino.append(inicio)
+    camino.reverse()
+
+    return camino, nodos_visitados, costos[meta]
+
+
+
+
 def mostrar_laberinto(laberinto, camino):
     camino_set = set(camino) if camino else set()
- #COLORE S Y G PARA DESTACARLOS 
+
     for i, fila in enumerate(laberinto):
         for j, celda in enumerate(fila):
             pos = (i, j)
@@ -158,46 +212,51 @@ def mostrar_laberinto(laberinto, camino):
                 print(YELLOW + 'S' + RESET, end='')
             elif celda == 'G':
                 print(RED + 'G' + RESET, end='')
+                #se cambio '.' a celda para que los 3 algoritmos puedan imprimir todos los simbolos
             elif pos in camino_set:
-                print(GREEN + '.' + RESET, end='')
+                print(GREEN + celda + RESET, end='') 
             else:
                 print(GRAY + celda + RESET, end='')
         print()
 
 
-# =========================
-# EJECUTAR ALGORITMO
-# =========================
-def ejecutar(algoritmo, nombre): #ejecuta el algoritmo seleccionado
+def ejecutar(algoritmo, nombre):
     lab = cargar_laberinto(laberinto_actual)
 
     inicio = time.perf_counter()
-    camino, nodos = algoritmo(lab) #ejecuta el algoritmo seleccionado
+    
+    #actualizacion en ejecutar para poder usas ucs
+    if nombre == "UCS":
+        camino, nodos, costo = algoritmo(lab)
+    else:
+        camino, nodos = algoritmo(lab)
+        costo = costo_camino(lab, camino)
+
     fin = time.perf_counter()
 
     print(f"\n== {nombre} ==")
     if camino:
-        print(f"Longitud de la ruta: {len(camino) - 1}") #resta 1 para no contar el nodo inicial
+        print(f"Longitud de la ruta: {len(camino) - 1}")
+        print(f"Costo total: {costo}") #mostrar costo del camino
     else:
         print("No se encontró solución")
 
     print(f"Nodos visitados: {nodos}")
-    print(f"Tiempo: {(fin - inicio) * 1000:.2f} ms\n") #tiempo en milisegundos
+    print(f"Tiempo: {(fin - inicio) * 1000:.2f} ms\n")
 
-    mostrar_laberinto(lab, camino) #muestra el laberinto con el camino encontrado
+    mostrar_laberinto(lab, camino)
 
 
-# =========================
-# MENÚ PRINCIPAL
-# =========================
+
 laberinto_actual = LABERINTO_DEFAULT
 
 while True:
     print("\n=== Menú Principal ===")
     print("1) Resolver con BFS")
     print("2) Resolver con DFS")
-    print("3) Cambiar laberinto (ruta de archivo)")
-    print("4) Salir")
+    print("3) Resolver con UCS")
+    print("4) Cambiar laberinto (ruta de archivo)")
+    print("5) Salir")
 
     opcion = input("Elige una opción: ")
 
@@ -206,17 +265,20 @@ while True:
 
     elif opcion == '2':
         ejecutar(dfs, "DFS")
-
+        
     elif opcion == '3':
+        ejecutar(ucs, "UCS")
+
+    elif opcion == '4':
         ruta = input("Ruta del archivo: ")
         try:
-            with open(ruta, 'r') as f: #abre el archivo en modo lectura
+            with open(ruta, 'r') as f:
                 laberinto_actual = f.read().strip()
             print("Laberinto cargado correctamente.")
         except:
             print("Error al leer el archivo.")
 
-    elif opcion == '4':
+    elif opcion == '5':
         print("Saliendo...")
         break
 
